3b68c9044ba2fd4a00153502fbbe316fc979cbf7
MD-DaniyalJavadia <daniyaljavadiaworks@gmail.com>
2025-08-17 20:25:32 +0500

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ import { Route } from 'react-router-dom'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <BrowserRouter>
--- src/components/formutilis.jsx
+++ src/components/formutilis.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function formutilis({title}) {
+
+    const [text,settext]=useState("Enter Your Text");
+
+    const handleClick=()=>{
+        console.log("Click");
+        let updatetext=text.toUpperCase()
+        settext(updatetext);
+    }
+    const handleChange=(event)=>{
+        settext(event.target.value)
+    }
     return (
         <React.Fragment>
             <div className="container mt-5">
 
                 <div className="mb-3">
                     <h1>{title}</h1>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleChange} id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
-                <button className='btn btn-primary'>Uppercase</button>
+                <button className='btn btn-primary' onClick={handleClick} >Uppercase</button>
             </div>
         </React.Fragment>
     )
